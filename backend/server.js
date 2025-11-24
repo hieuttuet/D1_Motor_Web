116ea4ba33d0e4723ea4c243053681c2f775f246
hieuttuet <hieuit@partron.co.kr>
2025-11-24 19:45:58 +0700

    update 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
